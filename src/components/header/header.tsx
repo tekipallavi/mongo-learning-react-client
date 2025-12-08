c4ba3b0fb26ebc4965ecde2ff5c6198799c61694
tekipallavi <tekipallavi@gmail.com>
2025-12-08 11:06:24 +0000

    css changes added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -17,7 +17,7 @@ const HeaderComponent = () => {
         setValue(newValue);
     };
     return (
-        <div className="header-container flex place-content-between">
+        <div className="header-container flex place-content-between mx-4">
             <div className="tabs-container">
                 <Tabs value={value} onChange={handleChange} aria-label="all-tabs">
                     <Tab className="text-xs! font-bold!" label="Home" {...addProps(0)} />
@@ -25,7 +25,7 @@ const HeaderComponent = () => {
                     <Tab className="text-xs! font-bold!" label="Notifications" {...addProps(2)} />
                 </Tabs>
             </div>
-            <div className="search-login-container flex">                            
+            <div className="search-login-container flex items-center gap-2">                            
                     <TextField id="outlined-basic" variant="outlined"  placeholder='Home'
                         slotProps={{
                             input: {
@@ -33,8 +33,11 @@ const HeaderComponent = () => {
                                     <InputAdornment position="start">
                                         <Search />
                                     </InputAdornment>
-                                )
+                                ),
                             },
+                            htmlInput: {                                
+                                className: 'p-1!'
+                            }
                         }}
                     />
                     <AccountCircle></AccountCircle>
